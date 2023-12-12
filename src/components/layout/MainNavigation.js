bf108fa054b5f164103f418b8d5afc0612eb530b
AonghusOD <aonghusod@hotmail.com>
2023-12-12 06:47:45 +0000

    NavBAr Transparent with Logo

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,24 +1,42 @@
 import { Link } from "react-router-dom";
-import classes from "./MainNavigation.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from '../../images/logo.svg'; 
+
 
 export default function MainNavigation(props) {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>BookABook.com</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home Page</Link>
-          </li>
-          {/* <li>
-            <Link to="/addnewbook">Add New Book</Link>
-          </li>
-          <li>
-            <Link to="/reservedbooks">ReservedBooks</Link>
-          </li> */}
-         
-        </ul>
-      </nav>
+    <header className="navbar navbar-expand-lg navbar-light" 
+            style={{ 
+                position: 'absolute', 
+                top: 0, 
+                left: 0, 
+                width: '100%', 
+                zIndex: 1000, 
+                backgroundColor: 'transparent !important' // Using !important to override any other styles
+            }}>
+      <div className="container-fluid">
+      <Link className="navbar-brand" to="/">
+          <img src={Logo} alt="Filipino Adventures" height="100" />
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home Page</Link>
+            </li>
+            {/* Uncomment and update these links as per your routing needs
+            <li className="nav-item">
+              <Link className="nav-link" to="/addnewbook">Add New Book</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/reservedbooks">ReservedBooks</Link>
+            </li>
+            */}
+          </ul>
+        </div>
+      </div>
     </header>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeadImage from '../images/Hero.png'; // Adjust the path as necessary
+
+
 
 
 export default function () {
-    
+
     return (
         <div className="App">
-           <h1>Hello WOrld!</h1>
-           </div>
+            <img
+                src={HeadImage}
+                className="img-fluid" // Bootstrap class for full-width image
+                alt="Filipino Adventure"
+                style={{ width: '100%', height: 'auto' }}
+            />
+            <h1>Hello WOrld!</h1>
+        </div>
     );
 }
