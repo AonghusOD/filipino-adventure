f9f364fada07fab9aad1caea684587ef8ceac470
AonghusOD <aonghusod@hotmail.com>
2023-12-12 06:15:11 +0000

    Added cart page with context

---
+++ src/pages/Cart.js
@@ -0,0 +1,16 @@
+import React, { useContext, useState, useEffect } from "react";
+import CartContext from "../context/CartContext";
+
+
+
+const Cart = () => {
+  const [cart, setCart] = useContext(CartContext);
+  
+
+
+  const history = useHistory();
+
+
+
+}
+export default Cart;
